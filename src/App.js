c1e4b8173520e3877ae749dd17c81beaaccc60ca
Laurenz04 <lstobbe@architrave.de>
2020-01-30 15:52:17 +0100

    title

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-import React, { Component } from 'react'
-import Table from './Table'
-
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <table>
-        	<TableHeader />
-        	<TableBody />
-        </table>
-      </div>
-    )
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
   }
-}
+}));
 
-export default App
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            NBA
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<h1>Hello, React!</h1>
-			</div>
-		)
-	}
-}
+import App from './App.js'
 
 ReactDOM.render(<App />, document.getElementById('root'))
