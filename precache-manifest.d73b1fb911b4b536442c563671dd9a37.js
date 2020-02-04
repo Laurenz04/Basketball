a815946741c1a3273e88bdec0165215e95d3e191
Laurenz04 <60353803+Laurenz04@users.noreply.github.com>
2020-02-04 12:21:36 +0000

    Deploying to gh-pages from  - 71c011ad1f30e58ace1a0a703531445fecc59e08 🚀

---
+++ precache-manifest.d73b1fb911b4b536442c563671dd9a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2b59e7835b205d817a31ddb4f693cd",
+    "url": "/Basketball/index.html"
+  },
+  {
+    "revision": "5fc1a8ad42e9ae2bec9a",
+    "url": "/Basketball/static/css/main.a722dd24.chunk.css"
+  },
+  {
+    "revision": "9cf2672ab08ab50bdcfc",
+    "url": "/Basketball/static/js/2.76e5c2a1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/Basketball/static/js/2.76e5c2a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fc1a8ad42e9ae2bec9a",
+    "url": "/Basketball/static/js/main.c3a6bbef.chunk.js"
+  },
+  {
+    "revision": "45298d2a4fda2206a9ca",
+    "url": "/Basketball/static/js/runtime-main.83fd6e68.js"
+  }
+]);
